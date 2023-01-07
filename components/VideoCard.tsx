6dcf1805c0670ab1d93bbd35fe8455f2acc60882
Kairat <asyl06kair@gmail.com>
2023-01-07 23:12:37 +0600

    added link to videoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -8,9 +8,13 @@ import Image from "next/legacy/image";
 import Link from "next/link";
 interface IProps {
   post: Video;
+  isShowingOnHome?: boolean;
 }
 
-const VideoCard: NextPage<IProps> = ({ post }) => {
+const VideoCard: NextPage<IProps> = ({
+  post: { caption, postedBy, video, _id, likes },
+  isShowingOnHome,
+}) => {
   const [isHover, setIsHover] = useState(false);
   const [playing, setPlaying] = useState(false);
   const [isVideoMuted, setIsVideoMuted] = useState(false);
@@ -25,22 +29,22 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
     }
   };
   useEffect(() => {
-    if (post && videoRef?.current) {
+    if (videoRef?.current) {
       videoRef.current.muted = isVideoMuted;
     }
-  }, [post, isVideoMuted]);
+  }, [isVideoMuted]);
   return (
     <div className="flex flex-col border-b-2 border-gray-200 pb-6">
       <div>
         <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
           <div className="md:w-16 md:h-16 w-10 h-10">
-            <Link href="/">
+            <Link href={`/profile/${postedBy?._id}`}>
               <>
                 <Image
                   width={62}
                   height={62}
                   className="rounded-full"
-                  src={post.postedBy.image}
+                  src={postedBy.image}
                   alt="profile photo"
                   layout="responsive"
                   priority
@@ -49,14 +53,14 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             </Link>
           </div>
           <div>
-            <Link href="/">
+            <Link href={`/profile/${postedBy?._id}`}>
               <div className="flex items-center gap-2">
                 <p className="flex gap-2 items-center md:text-md font-bold text-primary">
-                  {post.postedBy.userName}
+                  {postedBy.userName}
                   <GoVerified className="text-blue-400 text-md" />
                 </p>
                 <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
-                  {post.postedBy.userName}
+                  {postedBy.userName}
                 </p>
               </div>
             </Link>
@@ -69,10 +73,10 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
           onMouseEnter={() => setIsHover(true)}
           onMouseLeave={() => setIsHover(false)}
         >
-          <Link href={`/detail/${post._id}`}>
+          <Link href={`/profile/${postedBy?._id}`}>
             <video
               ref={videoRef}
-              src={post.video.asset.url}
+              src={video.asset.url}
               loop
               className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
             ></video>
